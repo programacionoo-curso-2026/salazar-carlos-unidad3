1f3948ec23c4885466e765b6813c56c988d51e2b
Usuario Internacional <usuario@uide.edu.ec>
2026-07-21 09:40:43 -0500

    Se agrego la primera version del ShowGoroutines

--- taller20-goroutines/main.go
+++ taller20-goroutines/main.go
@@ -1,73 +1,11 @@
 package main
 
-import ("fmt"
+import "fmt"
 
-        "time"
-
-)
-
- 
-
-func ShowGoroutine(id int) {
-
-    fmt.Print("Goroutine #%d\n", id)
-
+func main() {
+	ShowGoroutines(1)
 }
 
-func main(){
-
-     ShowGoroutine(1)
-
+func ShowGoroutines(id int) {
+	fmt.Printf("Goroutines #%d\n", id)
 }
-
-func main(){
-
-    go ShowGoroutine(1)
-
-}
-
-func main(){
-
-    go ShowGoroutine(1)
-
-    time.Sleep(1 * time.Second)
-
-}
-
-package main
-
-Import ("fmt"
-
-        "math/rand"
-
-        "time"
-
-)
-
- 
-
-func ShowGoroutine(id int) {
-
-    delay := rand.Intn(500)
-
-    fmt.Print("Goroutine #%d\n with %dms\n", id, delay)
-
-    time.Sleep(time.Milliseconds * time.Duration(delay))
-
-    
-
-}
-
-func main(){
-
-    for i :=0; i < 10; i++ {
-
-        go ShowGoroutine(1)
-
-    }
-
-    time.Sleep(time.Minute)
-
-}
-
- 
