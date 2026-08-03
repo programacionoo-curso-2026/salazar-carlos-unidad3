3a3c2626cbe7c785912fa758e268ac94e5cf6636
Usuario Internacional <usuario@uide.edu.ec>
2026-08-03 10:40:06 -0500

    Iteracion 2

--- taller23-goroutiness/main.go
+++ taller23-goroutiness/main.go
@@ -17,3 +17,27 @@ var (
   totalUpdates int
   updateMutex  sync.Mutex
 )
+
+func main_n() {
+  var wg sync.WaitGroup
+  wg.Add(3)
+  orders := generateOrders(20)
+  //go func() {
+  //  defer wg.Done()
+  //  processOrders(orders)
+  //}()
+  for i := 0; i < 3; i++ {
+    go func() {
+      defer wg.Done()
+      for _, order := range orders {
+        updateOrderStatus(order)
+      }
+    }()
+  }
+  //reportOrderStatus(orders)
+
+  wg.Wait()
+
+  fmt.Print("Todas las operaciones completadas. Saliendo\n")
+  fmt.Printf("Total Actualizaciones %d\n", totalUpdates)
+}
