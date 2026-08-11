4fa7b4213c82891159e33969093e536e2256ae7d
Usuario Internacional <usuario@uide.edu.ec>
2026-08-11 09:50:04 -0500

    Deber 4

---
+++ Deber4-DataAccessObject/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"CompetenciasDocentes/dao"
+	"CompetenciasDocentes/dataaccess"
+	"log"
+)
+
+func main() {
+	// Inicializar la base de datos
+	db := dataaccess.InitDB()
+	defer db.Close() // Importante: cerrar la conexión al final
+
+	log.Println("Base de datos inicializada correctamente")
+
+	// Crear el DAO
+	docenteDAO := dao.NewDocenteDAO(db)
+
+	// Crear la tabla
+	if err := docenteDAO.CreateTable(); err != nil {
+		log.Fatalf("Error al crear tabla: %v", err)
+	}
+}
